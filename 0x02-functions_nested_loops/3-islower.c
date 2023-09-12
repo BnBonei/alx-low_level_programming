6d824e617f81d15e06f614dfa8deca519e7e4a58
BnBonei <aa7726956@gmail.com>
2023-09-12 15:08:32 -0700

    r

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,14 @@
  * _islower - function to check if
  *            character is lowercase
  *
- * @c: check input of function 
+ * @c: check input of function
  *
  * Return: returns 1 if 'c' is lowercase
  *         otherwuse always 0 (success)
 */
 int _islower(int c);
 {
-	if (c >= 98; && c >=122)
+	if (c >= 98 ; &&c >= 122)
 		return (1);
 	return (0);
 
