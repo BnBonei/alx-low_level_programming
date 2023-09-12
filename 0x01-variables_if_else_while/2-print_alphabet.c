1ea858bc01558c9d077073ba7e19eae669df8538
BnBonei <aa7726956@gmail.com>
2023-09-11 20:12:30 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: printf all alphabrt
+ *
+ * Return: always  0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+									          ch++;
+
+	}
+	putchar('\n');
+
+	return (0);
+}
